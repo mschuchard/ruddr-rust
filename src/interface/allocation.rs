7ed0aa4d9f8809da0ef7f696de444820e067439f
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-04-15 09:32:34 -0400

    add unit tests for allocation interfaces

--- src/interface/allocation.rs
+++ src/interface/allocation.rs
@@ -7,7 +7,7 @@ use crate::model::types;
 
 /// Retrieves a specific Ruddr Allocation object by id, and deserializes it to the corresponding struct.
 /// ```ignore
-/// let allocation = allocation(&client, Some(types::UUID::from("212b8272-ed2a-4a91-950a-8a06b3546144"))).await?;
+/// let allocation = allocation(&client, types::UUID::from("212b8272-ed2a-4a91-950a-8a06b3546144")).await?;
 /// ```
 pub async fn allocation(
     client: &client::Client,
@@ -23,7 +23,7 @@ pub async fn allocation(
 /// ```ignore
 /// let allocations = allocations(
 ///     &client,
-///     Some(allocation::Assignment::Project),
+///     Some(allocation::AssignmentType::Project),
 ///     Some(types::UUID::from("ec5543de-3b0f-47a0-b8ef-a6e18dc4b885")),
 ///     Some(types::Date::from("2024-01-01")),
 ///     Some(types::Date::from("2024-01-01")),
@@ -57,4 +57,56 @@ pub async fn allocations(
     Ok(client
         .read::<allocation::Allocations>("allocations", &params)
         .await?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_allocation() {
+        let test = async {
+            let client = client::Client::new(Some("abcdefghi123456789"))
+                .await
+                .expect("client with token could not be constructed");
+            assert_eq!(
+                allocation(
+                    &client,
+                    types::UUID::from("212b8272-ed2a-4a91-950a-8a06b3546144")
+                )
+                .await
+                .unwrap_err()
+                .to_string(),
+                "client read response failed",
+                "allocation retrieval did not fail on auth",
+            )
+        };
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(test);
+    }
+
+    #[test]
+    fn test_allocations() {
+        let test = async {
+            let client = client::Client::new(Some("abcdefghi123456789"))
+                .await
+                .expect("client with token could not be constructed");
+            assert_eq!(
+                allocations(
+                    &client,
+                    Some(allocation::AssignmentType::Project),
+                    Some(types::UUID::from("ec5543de-3b0f-47a0-b8ef-a6e18dc4b885")),
+                    Some(types::Date::from("2024-01-01")),
+                    Some(types::Date::from("2024-01-01")),
+                )
+                .await
+                .unwrap_err()
+                .to_string(),
+                "client read response failed",
+                "allocations retrieval did not fail on auth",
+            )
+        };
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(test);
+    }
 }
