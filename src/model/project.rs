5634c22fa261e011cb65a94d2e8b29e2d59abd6f
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-04-30 15:37:15 -0400

    cargo fmt did not automatically re-exec after 2024 update

--- src/model/project.rs
+++ src/model/project.rs
@@ -245,73 +245,75 @@ mod tests {
         let project_deserialized = serde_json::from_str::<Project>(json_input)
             .expect("time entry could not be deserialized");
         let project = Project {
-              id: types::UUID(String::from("095e0780-48bf-472c-8deb-2fc3ebc7d90c")),
-              key: types::Slug(String::from("vendor-portal")),
-              name: String::from("Vendor Portal"),
-              notes: String::from("The client would like to develop a mobile app that rewards its customers for repeat purchases."),
-              status_id: Status::InProgress,
-              start: Some(types::Date(String::from("2021-09-01"))),
-              end: Some(types::Date(String::from("2022-01-31"))),
-              code: String::from("P-2021-00068"),
-              po_number: String::from("DM-2021-02059"),
-              billing_type_id: BillingType::FixedRecurring,
-              is_billable: true,
-              currency: String::from("USD"),
-              revenue_recognition_method: Some(RevenueRecognitionMethod::Manual),
-              fixed_fee: None,
-              fixed_recurring_fee: None,
-              fixed_recurring_start: None,
-              fixed_recurring_end: None,
-              use_roles: true,
-              use_budget: true,
-              budget_mode: Some(BudgetMode::Detailed),
-              use_monthly_budget: true,
-              monthly_budget_mode: Some(MonthlyBudgetMode::Detailed),
-              requires_notes: true,
-              requires_tasks: true,
-              record_status_id: RecordStatus::Active,
-              is_productive: None,
-              created_at: types::Timestamp(String::from("2022-03-15T14:59:18.825Z")),
-              client: Client{
+            id: types::UUID(String::from("095e0780-48bf-472c-8deb-2fc3ebc7d90c")),
+            key: types::Slug(String::from("vendor-portal")),
+            name: String::from("Vendor Portal"),
+            notes: String::from(
+                "The client would like to develop a mobile app that rewards its customers for repeat purchases.",
+            ),
+            status_id: Status::InProgress,
+            start: Some(types::Date(String::from("2021-09-01"))),
+            end: Some(types::Date(String::from("2022-01-31"))),
+            code: String::from("P-2021-00068"),
+            po_number: String::from("DM-2021-02059"),
+            billing_type_id: BillingType::FixedRecurring,
+            is_billable: true,
+            currency: String::from("USD"),
+            revenue_recognition_method: Some(RevenueRecognitionMethod::Manual),
+            fixed_fee: None,
+            fixed_recurring_fee: None,
+            fixed_recurring_start: None,
+            fixed_recurring_end: None,
+            use_roles: true,
+            use_budget: true,
+            budget_mode: Some(BudgetMode::Detailed),
+            use_monthly_budget: true,
+            monthly_budget_mode: Some(MonthlyBudgetMode::Detailed),
+            requires_notes: true,
+            requires_tasks: true,
+            record_status_id: RecordStatus::Active,
+            is_productive: None,
+            created_at: types::Timestamp(String::from("2022-03-15T14:59:18.825Z")),
+            client: Client {
                 id: types::UUID(String::from("d5afaffe-09e5-4d73-b02c-905b40fc6c22")),
                 name: String::from("Acme Company"),
-              },
-              practice: Practice{
+            },
+            practice: Practice {
                 id: types::UUID(String::from("40f95471-7f7c-4ffa-b838-8dcccab0f54a")),
                 name: String::from("Digital Transformation"),
-              },
-              project_type: ProjectType{
+            },
+            project_type: ProjectType {
                 id: types::UUID(String::from("9b0927a6-35a1-4795-a4ca-10167b05f7de")),
                 name: String::from("Content Management"),
-              },
-              tags: vec![
-                Tag{
-                  id: types::UUID(String::from("626db436-98bf-40cb-9937-c382af5d818c")),
-                  name: String::from("Atlanta Office"),
+            },
+            tags: vec![
+                Tag {
+                    id: types::UUID(String::from("626db436-98bf-40cb-9937-c382af5d818c")),
+                    name: String::from("Atlanta Office"),
                 },
-                Tag{
-                  id: types::UUID(String::from("9f26fb15-23f1-49a6-8558-c19ad4338472")),
-                  name: String::from("Data Analytics"),
+                Tag {
+                    id: types::UUID(String::from("9f26fb15-23f1-49a6-8558-c19ad4338472")),
+                    name: String::from("Data Analytics"),
                 },
-              ],
-              budget: Some(Budget{
+            ],
+            budget: Some(Budget {
                 revenue: 602500,
                 services_revenue: 600000,
                 other_revenue: 2000,
                 billable_expenses: 500,
                 non_billable_expenses: 150,
                 billable_hours: 150,
-                non_billable_hours: 10
-              }),
-              monthly_budget: Some(MonthlyBudget{
+                non_billable_hours: 10,
+            }),
+            monthly_budget: Some(MonthlyBudget {
                 revenue: 51220,
                 services_revenue: 50000,
                 other_revenue: 920,
                 billable_expenses: 300,
                 non_billable_expenses: 50,
                 billable_hours: 10,
-                non_billable_hours: 2
-              }),
+                non_billable_hours: 2,
+            }),
         };
         assert_eq!(
             project_deserialized, project,
