b06afef3dadd641abc1a7bb0a3c0e0ee87b5682c
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-06-27 10:44:53 -0400

    add expense report interface

---
+++ src/interface/expense_report.rs
@@ -0,0 +1,35 @@
+//! # Expense Report
+//!
+//! `interface::expense_report` consists of functions for interfacing with the Ruddr Expense Report endpoints.
+use crate::client::client;
+use crate::model::expense_report;
+use crate::model::types;
+
+/// Retrieves a specific Ruddr Expense Report object by id, and deserializes it to the corresponding model struct.
+/// https://ruddr.readme.io/reference/get-an-expense-report
+/// ```ignore
+/// let expense_report = expense_report(&client, types::UUID::from("2bdab00d-86fb-46dc-ae05-7cc9c4aedc80")).await?;
+/// ```
+pub async fn expense_report(
+    client: &client::Client,
+    id: types::UUID,
+) -> Result<expense_report::ExpenseReport, Box<dyn std::error::Error>> {
+    // retrieve expense report
+    Ok(client
+        .read::<expense_report::ExpenseReport>(&format!("expense-reports/{id}"), "")
+        .await?)
+}
+
+/// Retrieves all Ruddr Expense Report objects, and deserializes it to the corresponding vector of model structs.
+/// https://ruddr.readme.io/reference/list-expense-reports
+/// ```ignore
+/// let expense_reports = expense_reports(&client, Some("Joe"), Some("foo@bar.com")).await?;
+/// ```
+pub async fn members(
+    client: &client::Client,
+) -> Result<expense_report::ExpenseReports, Box<dyn std::error::Error>> {
+    // retrieve expense reports
+    Ok(client
+        .read::<expense_report::ExpenseReports>("members", &"limit=100")
+        .await?)
+}
